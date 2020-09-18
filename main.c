1ccaf4d5e54f87c077faf7143e0089911fb53292
samzolin <saz5193@psu.edu>
2020-09-18 20:24:47 +0000

    added line

--- main.c
+++ main.c
@@ -1,22 +1,20 @@
 //Author: Samantha Zolin
 //Section: 10
 #include <stdio.h>
-#include <math.h>
 #include <readline/readline.h>
-#include <string.h>
 #include <stdlib.h>
 
-int digit_sum(int n) {
-  if (n == 0) {
-    return 0;
+int digit_sum(n){
+  if (n<10) {
+    return n;
   }
   else {
-    return (n%10+digit_sum(n/10));
+    return n%10 + digit_sum(n/10);
   }
 }
 
-int main(digit_sum(int n)) {
-  int N=atoi(readline("Enter an int: "));
-  printf("sum of digits of {n} is {digit_sum(n)}.";
-
+int main(void){
+  int n = atoi(readline("Enter an int: "));
+  printf("sum of digits of %i is %i.\n", n, digit_sum(n));
 }
+
